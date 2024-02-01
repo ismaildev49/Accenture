7f3059ee9535129fe786604ae7a6486d1fd4dcce
ismaildev49 <ismaelbentatou@gmail.com>
2024-02-01 12:05:26 +0100

    corrected class to className

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,16 +2,16 @@ import LoginForm from '../components/loginPage/LoginForm.jsx'
 const Login = () => {
     return <>
     <div className="login-body">
-    <div class="container">
-	<div class="screen">
-		<div class="screen__content">
+    <div className="container">
+	<div className="screen">
+		<div className="screen__content">
 			<LoginForm></LoginForm>
 		</div>
-		<div class="screen__background">
-			<span class="screen__background__shape screen__background__shape4"></span>
-			<span class="screen__background__shape screen__background__shape3"></span>		
-			<span class="screen__background__shape screen__background__shape2"></span>
-			<span class="screen__background__shape screen__background__shape1"></span>
+		<div className="screen__background">
+			<span className="screen__background__shape screen__background__shape4"></span>
+			<span className="screen__background__shape screen__background__shape3"></span>		
+			<span className="screen__background__shape screen__background__shape2"></span>
+			<span className="screen__background__shape screen__background__shape1"></span>
 		</div>		
 	</div>
 </div>
