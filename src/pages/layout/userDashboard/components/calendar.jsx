6f8cabc0c28ae2c11f5c577c69ad960ba443c16b
ismaildev49 <ismaelbentatou@gmail.com>
2024-02-13 11:57:06 +0100

    ...

--- src/pages/layout/userDashboard/components/calendar.jsx
+++ src/pages/layout/userDashboard/components/calendar.jsx
@@ -12,56 +12,56 @@ import interactionPlugin from "@fullcalendar/interaction";
 // CALENDRIER end
 
 export default function Calendar() {
-    const infosUser = useContext(InfosUsersSession);
-    const [date, setDate] = useState("");
-    const [adresses, setAdresses] = useState([]);
-    const [worked, setWorked] = useState("");
-    const [selectedAdress, setSelectedAdress] = useState("");
-    const [workedError, setWorkedError] = useState("");
-    const [adressError, setAdressError] = useState("");
-    const [dates, setDates] = useState([]);
+  const infosUser = useContext(InfosUsersSession);
+  const [date, setDate] = useState("");
+  const [adresses, setAdresses] = useState([]);
+  const [worked, setWorked] = useState("");
+  const [selectedAdress, setSelectedAdress] = useState("");
+  const [workedError, setWorkedError] = useState("");
+  const [adressError, setAdressError] = useState("");
+  const [dates, setDates] = useState([]);
 
-    useEffect(() => {
+  useEffect(() => {
     getDates();
-    console.log(" infosUser ------------> " + JSON.stringify(infosUser));
-    }, []);
+    // console.log(" infosUser ------------> " + JSON.stringify(infosUser));
+  }, []);
 
-    useEffect(() => {
+  useEffect(() => {
     if (dates.length > 0) {
-        console.log("dates :", dates);
-        console.log("isEligible(dates) :", isEligible(dates));
-        updateEligibility()
+      console.log("dates :", dates);
+      console.log("isEligible(dates) :", isEligible(dates));
+      updateEligibility();
     }
 
     // console.log(JSON.parse(isEligible(dates)));
-    }, [dates, infosUser]);
-    const updateEligibility = async () => {
+  }, [dates, infosUser]);
+  const updateEligibility = async () => {
     try {
-        await database.updateDocument(
+      await database.updateDocument(
         import.meta.env.VITE_APP_DB_ID,
         import.meta.env.VITE_APP_USER_COLLECTION_ID,
         infosUser.$id,
         {
-            eligible: isEligible(dates),
+          eligible: isEligible(dates),
         }
-        );
+      );
     } catch (error) {
-        console.log("error while updating user eligibility :", error);
+      console.log("error while updating user eligibility :", error);
     }
-    };
-    const getAdresses = async () => {
+  };
+  const getAdresses = async () => {
     try {
-        const adress = await database.listDocuments(
+      const adress = await database.listDocuments(
         import.meta.env.VITE_APP_DB_ID,
         import.meta.env.VITE_APP_ADRESSES_COLLECTION_ID
-        );
-        console.log("result :", adress);
-        setAdresses(() => adress.documents);
+      );
+      console.log("result :", adress);
+      setAdresses(() => adress.documents);
     } catch (error) {
-        console.log("error while fetching adresses from db :", error);
+      console.log("error while fetching adresses from db :", error);
     }
-    };
-    function isEligible(array) {
+  };
+  function isEligible(array) {
     console.log("arrayyy", array);
 
     //1 Test si il faut JSON.parse etc...
@@ -70,305 +70,313 @@ export default function Calendar() {
     let top = 0;
     let test;
     for (let i = 0; i < array.length; i++) {
-        count = 0;
-        test = array[i];
-        for (let j = 0; j < array[i].length; j++) {
+      count = 0;
+      test = array[i];
+      for (let j = 0; j < array[i].length; j++) {
         if (test.eligible !== null && array[i] !== null) {
-            if (array[i].eligible === true) {
+          if (array[i].eligible === true) {
             count++;
-            }
+          }
         }
-        }
-        if (count > top) {
+      }
+      if (count > top) {
         top = count;
         adresseTop = test;
-        }
+      }
     }
     return test.eligible;
-    }
-    const getDates = async () => {
+  }
+  const getDates = async () => {
     try {
-        const dates = await database.listDocuments(
+      const dates = await database.listDocuments(
         import.meta.env.VITE_APP_DB_ID,
         import.meta.env.VITE_APP_DATES_COLLECTION_ID,
         [Query.equal("user", infosUser.$id), Query.limit(100)]
-        );
-        console.log("result :", dates.documents);
-        setDates(() => dates.documents);
+      );
+      console.log("result :", dates.documents);
+      setDates(() => dates.documents);
     } catch (error) {
-        console.log("error while fetching dates from db :", error);
+      console.log("error while fetching dates from db :", error);
     }
-    };
+  };
 
-    const handleClickCaseCalendrier = (infos) => {
+  const handleClickCaseCalendrier = (infos) => {
     console.log("infos user : ", infosUser);
     getAdresses();
     console.log(infos);
     console.log("type of date : ", typeof infos);
     setDate(() => infos);
     document.querySelector(".modal").style.display = "flex";
-    };
-    const handleWorkedChange = (e) => {
-    console.log("workedtargetvalue :", e.target.value);
+  };
+  const handleWorkedChange = (e) => {
+    
+
     const boolValue = e.target.value === "true";
+
     setWorked(() => boolValue);
+
+    console.log("e.target.value", e.target.value);
+
+    if (e.target.value == false) {
+    console.log("workedtargetvalue :", e.target.value);  
+      setSelectedAdress("didn't work");
+      document.querySelector("#adress").classList.add("didntWork");
+    }
 
     setWorkedError("");
     console.log("boolvalue", boolValue);
     // If no radio button is selected (neither "true" nor "false"), display an error
     if (boolValue !== true && boolValue !== false) {
-        setWorkedError("Please select if you worked or not");
+      setWorkedError("Please select if you worked or not");
     }
-    };
-    const handleSelectedAdress = (e) => {
+  };
+  const handleSelectedAdress = (e) => {
     const selectedAddressObject = JSON.parse(e.target.value);
     console.log("Selected address object:", selectedAddressObject);
     setSelectedAdress(() => selectedAddressObject);
-    };
-    const handleFormSubmit = async (e) => {
+  };
+  const handleFormSubmit = async (e) => {
     e.preventDefault();
 
     dates.forEach((element) => {
-        console.log("split from database date", element.date.split("T")[0], date);
-        console.log("user verification :", element.user, infosUser.$id);
+      console.log("split from database date", element.date.split("T")[0], date);
+      console.log("user verification :", element.user, infosUser.$id);
     });
 
     for (let i = 0; i < dates.length; i++) {
-        if (
+      if (
         dates[i].date.split("T")[0] === date &&
         dates[i].user.$id === infosUser.$id
-        ) {
+      ) {
         alert("You already filled this date, please select another date");
         window.location.reload();
-        }
+      }
     }
 
     // verification if all fields are filled
     if (!selectedAdress || !worked) {
-        // Display error message for each missing field
-        if (!selectedAdress) {
+      // Display error message for each missing field
+      if (!selectedAdress) {
         // Show error for selectedAdress
         setAdressError("Please select an adress");
-        }
-        if (worked !== true && worked !== false) {
+      }
+      if (worked !== true && worked !== false) {
         // Show error for worked
         setWorkedError("Please select if you worked or not");
-        }
+      }
     } else {
-        // If all fields are filled, calculate the distance between the user's home and the selected adress to know if the user is eligible or not.
-        console.log("home adress :", infosUser.homeAdress);
-        console.log("selected adress :", selectedAdress);
-        const home = {
+      // If all fields are filled, calculate the distance between the user's home and the selected adress to know if the user is eligible or not.
+      console.log("home adress :", infosUser.homeAdress);
+      console.log("selected adress :", selectedAdress);
+      const home = {
         lat: infosUser.geolocHome.split(",")[0],
         lng: infosUser.geolocHome.split(",")[1],
-        };
-        console.log("home :", home);
+      };
+      console.log("home :", home);
 
-        //if the user selected his home adress, the distance is 0
-        let distance;
-        if (selectedAdress === infosUser.homeAdress) {
+      //if the user selected his home adress, the distance is 0
+      let distance;
+      if (selectedAdress === infosUser.homeAdress) {
         distance = 0;
-        } else {
+      } else {
         const client = {
-            // GET GEOLOC
-            lat: selectedAdress.geolocation.split(",")[0],
-            lng: selectedAdress.geolocation.split(",")[1],
+          // GET GEOLOC
+          lat: selectedAdress.geolocation.split(",")[0],
+          lng: selectedAdress.geolocation.split(",")[1],
         };
         console.log("client geoloc ", client.lat, client.lng);
         distance = haversine(home.lat, home.lng, client.lat, client.lng);
         console.log("distance :", distance);
 
         console.log("home geoloc", home);
-        }
-        try {
+      }
+      try {
         await database
-            .createDocument(
+          .createDocument(
             import.meta.env.VITE_APP_DB_ID,
             import.meta.env.VITE_APP_DATES_COLLECTION_ID,
             "unique()",
             {
-                date: date,
-                clientAdress:
+              date: date,
+              clientAdress:
                 selectedAdress === infosUser.homeAdress
-                    ? infosUser.homeAdress
-                    : selectedAdress.fullAdress,
-                worked: worked,
-                user: infosUser.$id,
-                eligible: distance < 10 ? true : false,
+                  ? infosUser.homeAdress
+                  : selectedAdress.fullAdress,
+              worked: worked,
+              user: infosUser.$id,
+              eligible: distance < 10 ? true : false,
             }
-            )
-            .then((response) => {
+          )
+          .then((response) => {
             console.log("response :", response);
             alert("Data sent");
             window.location.reload();
-            });
-        } catch (error) {
+          });
+      } catch (error) {
         console.log("error while creating document in date table :", error);
-        }
-        setDate("");
-        setWorked("");
-        setSelectedAdress("");
-        setWorkedError("");
-        setAdressError("");
-        document.querySelector(".modal").style.display = "none";
+      }
+      setDate("");
+      setWorked("");
+      setSelectedAdress("");
+      setWorkedError("");
+      setAdressError("");
+      document.querySelector(".modal").style.display = "none";
     }
-    };
+  };
 
-    return (
+  return (
     <div className="calendrier">
-        <div className="modal">
+      <div className="modal">
         <i
-            className="fa-solid fa-xmark"
-            onClick={() => {
+          className="fa-solid fa-xmark"
+          onClick={() => {
             document.querySelector(".modal").style.display = "none";
-            }}
+          }}
         ></i>
         <form onSubmit={handleFormSubmit}>
-            <h3>{date}</h3>
-            <br />
-            <label>
+          <h3>{date}</h3>
+          <br />
+          <label>
             Did you work on {date} ?
             <div className="workedError">{workedError}</div>
             <br />
             <label>
-                <input
+              <input
                 type="radio"
                 name="worked"
                 placeholder="travaillé?"
                 value={"true"}
                 onChange={handleWorkedChange}
-                />
-                Yes
+              />
+              Yes
             </label>
             <br />
             <label>
-                <input
+              <input
                 type="radio"
                 name="worked"
                 placeholder="travaillé?"
                 value={"false"}
                 onChange={handleWorkedChange}
-                />
-                No
+              />
+              No
             </label>
-            </label>
-            <br />
-            <br />
+          </label>
+          <br />
+          <br />
 
-            <label>
+          <label>
             Where did you work on {date} ?
             <div className="adressError">{adressError}</div>
             <br />
             <select
-                name="adress"
-                id="adress"
-                defaultValue={""}
-                onChange={handleSelectedAdress}
+              name="adress"
+              id="adress"
+              defaultValue={""}
+              onChange={handleSelectedAdress}
             >
-                <option></option>
-                <option value={JSON.stringify(infosUser.homeAdress)}>Home</option>
+              <option></option>
+              <option value={JSON.stringify(infosUser.homeAdress)}>Home</option>
 
-                {adresses.map((adress) => {
-                console.log("adress :", adress);
+              {adresses.map((adress) => {
+                // console.log("adress :", adress);
 
                 return (
-                    <option
+                  <option
                     key={adress.$id}
                     className={adress.geolocation}
                     value={JSON.stringify(adress)}
-                    >
+                  >
                     {adress.clientName}
-                    </option>
+                  </option>
                 );
-                })}
+              })}
             </select>
-            </label>
+          </label>
 
-            <br />
-            <br />
-            <button className="btn">Send</button>
+          <br />
+          <br />
+          <button className="btn">Send</button>
         </form>
-        </div>
+      </div>
 
-        <div className="calendrier_content">
+      <div className="calendrier_content">
         <div className="eligible">
-            {
-            infosUser.eligible ? (
-                <p style={{ color: "green" }}>You are eligible for this month</p>
-            ) : (
-                <p style={{ color: "red" }}>You are not eligible for this month</p>
-            )
-            }
-            <br />
+          {infosUser.eligible ? (
+            <p style={{ color: "green" }}>You are eligible for this month</p>
+          ) : (
+            <p style={{ color: "red" }}>You are not eligible for this month</p>
+          )}
+          <br />
         </div>
         <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView={"dayGridMonth"}
-            headerToolbar={{
+          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+          initialView={"dayGridMonth"}
+          headerToolbar={{
             start: "title", // will normally be on the left. if RTL, will be on the right
             center: "",
             end: "", // will normally be on the right. if RTL, will be on the left
-            }}
-            events={dates.map((date) => {
+          }}
+          events={dates.map((date) => {
             return {
-                title: date.clientAdress,
-                date: date.date,
-                color: date.eligible ? "green" : "red",
+              title: date.clientAdress,
+              date: date.date,
+              color: date.eligible ? "green" : "red",
             };
-            })}
-            // locale='fr'
-            selectable={true}
-            dateClick={function (info) {
+          })}
+          // locale='fr'
+          selectable={true}
+          dateClick={function (info) {
             setDate(info.dateStr);
             handleClickCaseCalendrier(info.dateStr);
-            }}
+          }}
         />
-        </div>
+      </div>
     </div>
-    );
+  );
 }
 
 export const handleGeocode = (address) => {
-    const formattedAddress = address.replace(/ /g, "+");
-    const url = `https://api.opencagedata.com/geocode/v1/json?q=${formattedAddress}&key=${
+  const formattedAddress = address.replace(/ /g, "+");
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${formattedAddress}&key=${
     import.meta.env.VITE_APP_API_KEY_GEOLOC
-    }`;
+  }`;
 
-    fetch(url)
+  fetch(url)
     .then((response) => response.json())
     .then((data) => {
-        const lat = data.results[0].geometry.lat;
-        const lng = data.results[0].geometry.lng;
+      const lat = data.results[0].geometry.lat;
+      const lng = data.results[0].geometry.lng;
     })
     .catch((error) => {
-        console.error("Error fetching geocoding data:", error);
+      console.error("Error fetching geocoding data:", error);
     });
 };
 
 export const haversine = (lat1, lon1, lat2, lon2) => {
-    const R = 6_371;
-    const deltaLat = (lat2 - lat1) * (Math.PI / 180);
-    const deltaLon = (lon2 - lon1) * (Math.PI / 180);
-    const a =
+  const R = 6_371;
+  const deltaLat = (lat2 - lat1) * (Math.PI / 180);
+  const deltaLon = (lon2 - lon1) * (Math.PI / 180);
+  const a =
     Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2) +
     Math.cos(lat1 * (Math.PI / 180)) *
-        Math.cos(lat2 * (Math.PI / 180)) *
-        Math.sin(deltaLon / 2) *
-        Math.sin(deltaLon / 2);
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    const distance = R * c; // Distance in kilometers
+      Math.cos(lat2 * (Math.PI / 180)) *
+      Math.sin(deltaLon / 2) *
+      Math.sin(deltaLon / 2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  const distance = R * c; // Distance in kilometers
 
-    return distance;
+  return distance;
 };
 
 export const handleCalculateDistance = () => {
-    if (coordinates1 && coordinates2) {
+  if (coordinates1 && coordinates2) {
     const distance = haversine(
-        coordinates1.lat,
-        coordinates1.lng,
-        coordinates2.lat,
-        coordinates2.lng
+      coordinates1.lat,
+      coordinates1.lng,
+      coordinates2.lat,
+      coordinates2.lng
     );
     setDistance(distance.toFixed(2)); // Round to 2 decimal places
-    }
+  }
 };
